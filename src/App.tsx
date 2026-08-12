c49ed50bcc416c9174254846b5355aac1bc34213
harelcn <h0586936991@gmail.com>
2026-08-12 23:16:20 +0300

    feat: wire up application routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateFolder from './pages/CreateFolder';
+import ScanFolder from './pages/ScanFolder';
+import JoinFolder from './pages/JoinFolder';
+import MyFolders from './pages/MyFolders';
+import FolderView from './pages/FolderView';
+
 export default function App() {
-  return <div>Wedding Cam</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/create" element={<CreateFolder />} />
+      <Route path="/scan" element={<ScanFolder />} />
+      <Route path="/join/:folderId" element={<JoinFolder />} />
+      <Route path="/my-folders" element={<MyFolders />} />
+      <Route path="/folder/:folderId" element={<FolderView />} />
+    </Routes>
+  );
 }
